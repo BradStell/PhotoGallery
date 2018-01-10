bf77a996b8e53e619ecfd5e261940a077aaea8b3
Brad Stell <bradstell87@gmail.com>
2018-01-10 16:51:13 -0500

    Main image in list correct layout

--- ClientApp/components/Home.tsx
+++ ClientApp/components/Home.tsx
@@ -5,7 +5,16 @@ export class Home extends React.Component<RouteComponentProps<{}>, {}> {
     public render() {
         return (
             <div className="home-page-wrapper">
-                <div className="main-image" />
+            <div style={{ display: 'flex', flexFlow: 'row nowrap'}}>
+                <button >Prev</button>
+                <button >Next</button>
+            </div>
+                <div className="carousel-wrapper">
+                    <ul className="img-carousel">
+                        <li className="main-image" style={{ opacity: 0, visibility: 'hidden', backgroundImage: `url('GalleryImages/dolmites.png')` }}/>
+                        <li className="main-image" style={{ opacity: 1, visibility: 'visible', backgroundImage: `url('GalleryImages/clt-pink.jpg')` }} />
+                    </ul>
+                </div>
                 <div className="footer" />
             </div>
         );
