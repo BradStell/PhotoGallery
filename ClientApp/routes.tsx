af299e53e1b67b42246e098ded6d9d9298699a3c
James Taylor <james.taylor@kingsmensoftware.com>
2018-01-17 11:47:32 -0500

    added admin route

--- ClientApp/routes.tsx
+++ ClientApp/routes.tsx
@@ -4,11 +4,13 @@ import { Layout } from './components/Layout';
 import { Home } from './components/Home';
 import { About } from './components/About';
 import { Galleries } from './components/Galleries';
+import { Admin } from './components/Admin';
 
 export const routes = (
     <Layout>
         <Route exact path='/' component={ Home } />
         <Route path='/galleries' component={ Galleries } />
         <Route path='/about' component={ About } />
+        <Route path='/admin' component={ Admin } />
     </Layout>
 );
