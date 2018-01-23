9f2838109eeaab307f7bf52be672dd99f5ae95e2
Brad Stell <bradstell87@gmail.com>
2018-01-23 18:12:16 -0500

    Added gallery seed data

--- SeedTestData.cs
+++ SeedTestData.cs
@@ -12,15 +12,25 @@ namespace PhotoGallery
         {
             using (var _db = new ApplicationDbContext(serviceProvider.GetRequiredService<DbContextOptions<ApplicationDbContext>>()))
             {
-                if (_db.Images.Any())
+                // Use this to remove all items from db when adding new seed data
+                // start app with this and saveContext un commented
+                // comment back out and uncomment seed data and run app
+                //_db.Images.RemoveRange(_db.Images);
+                //_db.Galleries.RemoveRange(_db.Galleries);
+
+                if (_db.Images.Any() || _db.Galleries.Any())
                 {
                     return;   // DB has been seeded
                 }
 
+                Guid lafotenGuid = Guid.NewGuid();
+                Guid dolomitesGuid = Guid.NewGuid();
+                Guid charlotteGuid = Guid.NewGuid();
+
                 _db.Images.AddRange(
                     new Image()
                     {
-                        Id = new Guid(),
+                        Id = lafotenGuid,
                         PathName = @"lafoten.jpg",
                         Title = @"Lafoten Islands",
                         IsCarouselImage = true,
@@ -28,7 +38,7 @@ namespace PhotoGallery
                     },
                     new Image()
                     {
-                        Id = new Guid(),
+                        Id = dolomitesGuid,
                         PathName = @"dolomites_landscape_photography_trecime_3.jpg",
                         Title = @"Dolomites Landscape",
                         IsCarouselImage = true,
@@ -36,7 +46,7 @@ namespace PhotoGallery
                     },
                     new Image()
                     {
-                        Id = new Guid(),
+                        Id = Guid.NewGuid(),
                         PathName = @"russian_winter.jpg",
                         Title = @"Russian Winter",
                         IsCarouselImage = true,
@@ -44,7 +54,7 @@ namespace PhotoGallery
                     },
                     new Image()
                     {
-                        Id = new Guid(),
+                        Id = Guid.NewGuid(),
                         PathName = @"dolomites_milkyway_photography_trecime_1.jpg",
                         Title = @"Dolomites Milkyway",
                         IsCarouselImage = true,
@@ -52,11 +62,35 @@ namespace PhotoGallery
                     },
                     new Image()
                     {
-                        Id = new Guid(),
+                        Id = charlotteGuid,
                         PathName = @"clt-pink.jpg",
                         Title = @"Charlotte Flamingo Sunset",
                         IsCarouselImage = true,
                         Gallery = null
+                    }
+                );
+
+                _db.Galleries.AddRange(
+                    new Gallery()
+                    {
+                        Id = Guid.NewGuid(),
+                        Title = "Daniel Kordan",
+                        CreateDateTime = new DateTime(),
+                        CoverImage_Id = lafotenGuid
+                    },
+                    new Gallery()
+                    {
+                        Id = Guid.NewGuid(),
+                        Title = "Michael Shainblum",
+                        CreateDateTime = new DateTime(),
+                        CoverImage_Id = dolomitesGuid
+                    },
+                    new Gallery()
+                    {
+                        Id = Guid.NewGuid(),
+                        Title = "Charlotte",
+                        CreateDateTime = new DateTime(),
+                        CoverImage_Id = charlotteGuid
                     }
                 );
 
@@ -65,51 +99,3 @@ namespace PhotoGallery
         }
     }
 }
-
-/*
-
-using (var context = new MvcMovieContext(
-                serviceProvider.GetRequiredService<DbContextOptions<MvcMovieContext>>()))
-            {
-                // Look for any movies.
-                if (context.Movie.Any())
-                {
-                    return;   // DB has been seeded
-                }
-
-                context.Movie.AddRange(
-                     new Movie
-                     {
-                         Title = "When Harry Met Sally",
-                         ReleaseDate = DateTime.Parse("1989-1-11"),
-                         Genre = "Romantic Comedy",
-                         Price = 7.99M
-                     },
-
-                     new Movie
-                     {
-                         Title = "Ghostbusters ",
-                         ReleaseDate = DateTime.Parse("1984-3-13"),
-                         Genre = "Comedy",
-                         Price = 8.99M
-                     },
-
-                     new Movie
-                     {
-                         Title = "Ghostbusters 2",
-                         ReleaseDate = DateTime.Parse("1986-2-23"),
-                         Genre = "Comedy",
-                         Price = 9.99M
-                     },
-
-                   new Movie
-                   {
-                       Title = "Rio Bravo",
-                       ReleaseDate = DateTime.Parse("1959-4-15"),
-                       Genre = "Western",
-                       Price = 3.99M
-                   }
-                );
-                context.SaveChanges();
-            }
-*/
